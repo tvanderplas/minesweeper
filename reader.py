b93bc25bca484f2ea2c567baa0ea66baba2bd6a5
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-05-14 20:26:43 -0400

    place old exploratory code behind __main__ check

--- reader.py
+++ reader.py
@@ -32,27 +32,29 @@ class Game_UI(object):
 	def clear(self, x, y):
 		pass
 
-def bgr_to_rgb(image: np.array):
-	red = np.copy(image[::, ::, 0])
-	image[::, ::, 0] = image[::, ::, 2]
-	image[::, ::, 2] = red
+if __name__ == '__main__':
+		
+	def bgr_to_rgb(image: np.array):
+		red = np.copy(image[::, ::, 0])
+		image[::, ::, 0] = image[::, ::, 2]
+		image[::, ::, 2] = red
 
-img = np.array(ImageGrab.grab())
+	img = np.array(ImageGrab.grab())
 
-board_w, board_h = 30, 16
-min_x, min_y, step, side = 215, 139, 50.25, 38
-x_index = [min_x + round(step * x) for x in range(board_w)]
-y_index = [min_y + round(step * y) for y in range(board_h)]
-board = [[img[y:y + side, x:x + side] for x in x_index] for y in y_index]
+	board_w, board_h = 30, 16
+	min_x, min_y, step, side = 215, 139, 50.25, 38
+	x_index = [min_x + round(step * x) for x in range(board_w)]
+	y_index = [min_y + round(step * y) for y in range(board_h)]
+	board = [[img[y:y + side, x:x + side] for x in x_index] for y in y_index]
 
-for i, row in enumerate(board):
-	for j, cell in enumerate(row):
-		a_cell = Image.fromarray(cell[::, ::])
-		# a_cell.save(str(randint(0, 99999)).zfill(5) + '.png', 'png')
+	for i, row in enumerate(board):
+		for j, cell in enumerate(row):
+			a_cell = Image.fromarray(cell[::, ::])
+			# a_cell.save(str(randint(0, 99999)).zfill(5) + '.png', 'png')
 
-bgr_to_rgb(img)
-# pylint: disable=no-member
-cv2.imshow("test", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# pylint: enable=no-member
+	bgr_to_rgb(img)
+	# pylint: disable=no-member
+	cv2.imshow("test", img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	# pylint: enable=no-member
